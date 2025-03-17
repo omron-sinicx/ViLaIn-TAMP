8dbfebb44a74ecad0317ebc92fba212a885b9460
Jeremy Siburian <jeremysiburian30@gmail.com>
2025-03-17 14:09:38 +0900

    [WIP] example end-to-end generation script

---
+++ scripts/main.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+import json
+import os
+import copy
+import base64
+from vilain import ViLaIn
+from vilain.vilain_utils import PDDLProblem
+
+
+def get_project_root():
+    """Get absolute path to project root directory."""
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def main():
+    """ Full example script of PD generation with ViLain."""
+    # Get project root directory and data paths
+    project_root = get_project_root()
+    data_dir = os.path.join(project_root, "data", "vilain_data", "cooking")
+
+    # PDDL domain and problems
+    pddl_domain_str = open(os.path.join(data_dir, "domain.pddl")).read()
+    
+
+    # PDDL domain and problems
+    pddl_domain_str = open(os.path.join(data_dir, "domain.pddl")).read()
+
+    pddl_problem_strs = [
+        open(os.path.join(data_dir, "problems", f"problem{i}.pddl")).read()
+        for i in range(1, 10+1)
+    ]
+
+    pddl_problems = [
+        PDDLProblem(pddl_problem_str)
+        for pddl_problem_str in pddl_problem_strs
+    ]
+
+    pddl_problem_obj_strs = [
+        pddl_problem.pddl_objects
+        for pddl_problem in pddl_problems
+    ]
+
+    pddl_problem_init_strs = [
+        pddl_problem.pddl_initial_state
+        for pddl_problem in pddl_problems
+    ]
+
+    pddl_problem_goal_strs = [
+        pddl_problem.pddl_goal
+        for pddl_problem in pddl_problems
+    ]
+
+    # instructions
+    instructions = [
+        open(os.path.join(data_dir, "instructions", f"problem{i}.txt")).read()
+        for i in range(1, 10+1)
+    ]
+    
+    ### Initialize ViLaIn
+    model = "gpt-4o-2024-11-20"
+    detection_model = "Qwen/Qwen2.5-VL-7B-Instruct"
+    detection_args = {
+        "base_url": "http://localhost:33333/v1",
+        "api_key": "qwen-2-5-vl-7b-instruct",
+    }
+    
+    vilain = ViLaIn(model, detection_args, detection_model)
+
+
+    # Getting bounding boxes for the fixed objects
+    # image (scene observation) paths
+    images = [
+        base64.b64encode(open(os.path.join(data_dir, "observations", f"problem{i}.jpg"), "rb").read()).decode("utf-8")
+        for i in range(1, 10+1)
+    ]
+
+    all_bboxes = [
+        [
+            (key, val["bbox"])
+            for key, val in json.load(open(os.path.join(data_dir, "annotated_bboxes", f"problem{i}.json"))).items()
+        ]
+        for i in range(1, 10+1)
+    ]
+
+    fixed_bboxes = copy.deepcopy(all_bboxes[-1])
+    obj_len = len(fixed_bboxes)
+
+    for i in reversed(range(obj_len)):
+        obj = fixed_bboxes[i][0]
+
+        if obj in ("cucumber", "carrot", "tomato", "cutting_board", "bowl"):
+            del fixed_bboxes[i]
+        else:
+            bbox = fixed_bboxes[i][1]
+            fixed_bboxes[i] = (obj, [ b / 512 for b in bbox ])
+
+    # Perform object detection using ViLaIn
+    detection_result = vilain.detect_objects(
+        images[-1],
+        fixed_bboxes,
+        "cooking",
+        (640, 640),
+        dummy_detection=True, ### Use dummy detection for testing
+    )
+
+    print("-" * 30)
+    print("### prompt:\n", detection_result["prompt"])
+    print("### bboxes:\n", detection_result["bboxes"])
+    print("### The generated objects:\n", detection_result["result"])
+    print()
+    # test initial state generation with example
+    result = vilain.generate_initial_state(
+        pddl_domain_str,
+        pddl_problem_obj_strs[0],
+        all_bboxes[0],
+        images[0],
+        [{
+            "pddl_problem_obj_str": pddl_problem_obj_strs[1],
+            "bboxes": all_bboxes[1],
+            "pddl_problem_init_str": pddl_problem_init_strs[1],
+        }],
+    )
+
+    print("-" * 30)
+    print("prompt:\n", result["prompt"])
+    print("The generated initial state with an example:\n", result["result"])
+    print()
+
+    
+if __name__ == "__main__":
+    main()
