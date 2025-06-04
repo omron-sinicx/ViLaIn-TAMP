1da2d9bf86ca95fb3a634d06c4e652b6f69501f1
Cristian Beltran <3798796+cambel@users.noreply.github.com>
2025-06-04 09:59:49 +0900

    Move and fix video size

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -9,27 +9,28 @@ export default class Video extends React.Component {
   render() {
     if (!this.props.video) return null;
     const wrapperClass = {
-      overflow: 'hidden',
-      paddingBottom: '56.25%',
       position: 'relative',
+      width: '100%',
+      paddingBottom: '76.7%', // 1080/1408 = 0.767 for 1408x1080 aspect ratio
       height: 0,
+      overflow: 'hidden',
+      backgroundColor: 'transparent',
     };
     const innerClass = {
-      left: 0,
-      top: 0,
-      height: '100%',
-      width: '100%',
       position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      border: 'none',
     };
     return (
       <div className="uk-section">
-        <h2 className="uk-heading-line uk-text-center" id="video">
-          Video
-        </h2>
+        <h2 className="uk-heading-line uk-text-center" id="video"></h2>
         <div style={wrapperClass}>
           <iframe
             style={innerClass}
-            className="uk-align-center uk-width-1-1"
+            className="uk-align-center"
             src={this.props.video}
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -66,12 +66,12 @@ class Template extends React.Component {
           header={data.header}
         />
         <div className="uk-container uk-container-small">
+          <Video video={data.resources.video} />
           <Overview
             overview={data.overview}
             teaser={data.teaser}
             description={data.description}
           />
-          <Video video={data.resources.video} />
           <SpeakerDeck dataId={data.speakerdeck} />
           <Body body={data.body} />
           <Contact
